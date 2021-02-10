3b1c3dcf92529cd88a66d22a1d8c9cf1b69ee1e1
liheng <1061555401@qq.com>
2021-02-10 21:53:08 +0800

    运算符

---
+++ HelloJava.java
@@ -0,0 +1,92 @@
+
+class HelloJava {
+  public static void main(String[] args) {
+    // System.out.println("HelloWorld");
+
+    // int myAge = 22;
+    // System.out.println(myAge);
+    // int hisAge;
+
+    // System.out.println(hisAge);
+
+    // byte b1 = 123;
+    // byte b2 = -128;
+    // b2 = 127;
+    // System.out.println(b2);
+
+    // short s1 = 1234;
+    // int i1 = 12341234;
+    // long d1 = 1234123412341234L;
+    // System.out.println(d1);
+
+    // double d1 = 1.23123;
+    // float f1 = 23423.234134132f;
+    // System.out.println(d1);
+    // System.out.println(f1);
+    // System.out.println('中');
+    // char c1 = '1';
+    // System.out.println('\u0043');
+
+    // boolean isMarried = false;
+    // if(isMarried) {
+    // System.out.println(isMarried);
+    // } else {
+    // System.out.println("\"single dog!\"");
+    // }
+
+    // System.out.println('a' + 1);
+
+    // byte b1 = 123;
+    // // int i1 = 1341234;
+    // // short b2 = i1 + b1;
+    // byte b2 = 123;
+    // byte b3 = (byte)(b2 + b1);
+    // System.out.println(b1);
+    // System.out.println(b2);
+
+    // double d1 = 12.6643242;
+    // int i1 = (int)d1;
+    // System.out.println(i1);
+
+    // System.out.println((byte)128);
+
+    // long l1 = 2314341234123434l;
+    // float f1 = 1.2341234123f;
+
+    // char c = ' ';
+    // String s = "";
+
+    // int i1 = 1234;
+    // String s1 = "what";
+    // boolean b1 = false;
+    // String s2 = i1 + s1 + b1;
+    // System.out.println(s2);
+    // System.out.println(true + 1);
+    // String s1 = 4;
+
+    // System.out.println(0127);
+    // System.out.println(0x123a);
+
+    // System.out.println((double)15/2);
+    // System.out.println((double)(15/2));
+
+    // System.out.println("what" instanceof String);
+
+    // System.out.println(false < true);
+
+    // int num1 = 10;
+    // int num2 = 20;
+    // num1 = num1 + num2;
+    // num2 = num1 - num2;
+    // num1 = num1 - num2;
+    // System.out.println(num1 + " " + num2);
+
+    // double i = (1 > 2)? 1.3: 23;
+    // int i2 = 1 > 2? "what": 13;
+    // char c = 211;
+    // String s = "123";
+    // int i = (int)s;
+    // System.out.println(c);
+
+  }
+}
---
+++ ScannerTest.java
@@ -0,0 +1,32 @@
+// import java.util.Scanner;
+// class ScannerTest {
+//   public static void main(String[] args) {
+//       Scanner scanner = new Scanner(System.in);
+//     System.out.print("请输入年龄：");
+//     int i = scanner.nextInt();
+
+//     System.out.println(i);
+//     System.out.println("请输入体重:" + scanner.nextDouble());
+//   }
+// }
+
+import java.util.Scanner;
+
+public class ScannerTest {
+  public static void main(String[] args) {
+    System.out.println(100.1234 <= 100);
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("请输入你的成绩：");
+    float grade = scanner.nextFloat();
+    if (grade == 100) {
+      System.out.println("BMW");
+    } else if (grade > 80) {
+      System.out.println("iphone xs max");
+    } else if (grade >= 60) {
+      System.out.println("ipad");
+    } else {
+      System.out.println("nothing");
+    }
+  }
+}
