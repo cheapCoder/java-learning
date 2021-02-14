9b46eacb470f45b4d878fb0040308196196829c2
liheng <1061555401@qq.com>
2021-02-15 01:05:23 +0800

    二维数组的定义

---
+++ array-4.java
@@ -0,0 +1,23 @@
+class array4 {
+  public static void main(String[] args) {
+    // int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+    // int[] arr3 = { 1, 213, 123, 1231, 12, 233 };
+    // System.out.println(arr.length);
+
+    // // int arr2[] = new int[5]; // NOTE:默认值
+    // // float[] arr2 = new float[4];
+    // // char[] arr2 = new char[4];
+    // // boolean[] arr2 = new boolean[4];
+    // String[] arr2 = new String[4];
+    // for (int i = 0; i < arr2.length; i++) { // NOTE:遍历
+    // System.out.println(arr2[i]);
+    // }
+
+    // NOTE: 二维数组
+    // int[][] arr = new int[2][3][];
+    // int[][] arr = new int[][]{{1,2,3,4}, {5,6,7,8,9}};
+    // int[][] arr = { { 1, 2, 3, 4 }, { 213, 1234, 234, 5 }};
+
+    System.out.println(arr[1][1]);
+  }
+}
