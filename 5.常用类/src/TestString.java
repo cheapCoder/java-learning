e4b8532dbb24b2b0a66bde092fb85b893a736efc
liheng <1061555401@qq.com>
2021-04-15 16:45:10 +0800

    String,StringBuffer,StringBuilder

---
+++ 5.常用类/src/TestString.java
@@ -0,0 +1,23 @@
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+public class TestString {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+
+//        String s1 = "what";
+//        String s2 = "whatwhat";
+//        System.out.println((s1 + "what").intern() == s2);
+//        System.out.println("abcdefg" == "abcdefg");
+//        System.out.println(String.valueOf(123) + 1);
+//        System.out.println("123".toString() + 1);
+
+//        String s = "abc中国";
+//        System.out.println(Arrays.toString(s.getBytes("gbk")));
+//        System.out.println(new String(s.getBytes("gbk"), "GBK"));
+//        System.out.println(new StringBuffer("abc").append(1).append("2").append('3').delete(3,4));
+//        System.out.println(System.currentTimeMillis());
+//        System.out.println(new Date(2234123412341L));
+        System.out.println(new java.sql.Date(123412341234L));
+    }
+}
+
